0fc7ba2b82c7663e722c79e395f5ebeae6cfdc62
Anusha <anushavalmiki.amaze@gmail.com>
2021-12-23 17:38:09 +0530

    aaa

--- src/app/scheduled-interviews/scheduled-interviews.component.ts
+++ src/app/scheduled-interviews/scheduled-interviews.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RecruitmentServiceService } from '../recruitment-service.service';
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -8,10 +11,108 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduledInterviewsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private RecriutmentServiceService:RecruitmentServiceService,private ActivatedRoute:ActivatedRoute) { }
+
+  
+  joblist: any;
+  search: any;
+  count: any;
+  staffid: any;
 
   ngOnInit(): void {
+    this.staffid = localStorage.getItem('staffssID');
+    this.GetCandidateReg()
+    // this.insertdetails()
   }
+
+  public GetCandidateReg() {
+    debugger
+    if (this.staffid == undefined) {
+      this.RecriutmentServiceService.GetCandidateRegistration().subscribe(data => {
+        this.joblist = data.filter(x => x.scheduled == 1 && x.interviewRejected == 0 && x.interviewSelected == 0);
+        this.count = this.joblist.length;
+      })
+    }
+    else {
+      this.RecriutmentServiceService.GetCandidateRegistration().subscribe(data => {
+        debugger
+        this.joblist = data.filter(x => x.scheduled == 1 && x.interviewRejected == 0 && x.interviewSelected == 0 && x.staffID == this.staffid);
+        this.count = this.joblist.length;
+      })
+
+    }
+
+  }
+
+
+
+
+  public Accept(id:any, rinterview:any) {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'You want Accept this!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, Accept it!',
+      cancelButtonText: 'No, keep it'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        debugger
+        this.RecriutmentServiceService.RejectInterview(id, 1, rinterview).subscribe(data => {
+          Swal.fire(
+            'Shortlisted!',
+            'Candidate has been Accepted',
+            'success'
+          )
+          this.GetCandidateReg()
+        })
+        // For more information about handling dismissals please visit
+        // https://sweetalert2.github.io/#handling-dismissals
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire(
+          'Cancelled',
+          'Your imaginary file is safe :)',
+          'error'
+        )
+      }
+    })
+  }
+
+
+  public Reject(id:any, interview:any) {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'You want Reject this!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, Accept it!',
+      cancelButtonText: 'No, keep it'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.RecriutmentServiceService.RejectInterview(id, 2, interview).subscribe(data => {
+          Swal.fire(
+            'Rejected!',
+            'Candidate has been Rejected',
+            'success'
+          )
+          this.GetCandidateReg()
+        })
+        // For more information about handling dismissals please visit
+        // https://sweetalert2.github.io/#handling-dismissals
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire(
+          'Cancelled',
+          'Your imaginary file is safe :)',
+          'error'
+        )
+      }
+    })
+  }
+
+  public GetOfferLetter(offer:any) {
+    window.open(offer, "_blank")
+  }
+
 
 
 
--- src/app/selected-candidates/selected-candidates.component.ts
+++ src/app/selected-candidates/selected-candidates.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RecruitmentServiceService } from '../recruitment-service.service';
+import { ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-selected-candidates',
   templateUrl: './selected-candidates.component.html',
@@ -6,22 +9,121 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectedCandidatesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private RecruitmentServiceService:RecruitmentServiceService,private ActivatedRoute:ActivatedRoute) { }
 
+
+ 
+  joblist: any;
+  count: any;
+  search: any;
+  date: any;
   ngOnInit(): void {
+    this.GetCandidateReg()
   }
 
-  files: File[] = [];
-  onSelect(event: { addedFiles: any; }) {
-    debugger
-    console.log(event);
-    this.files.push(event.addedFiles[0]);
-    console.log("content", this.files);
+  public GetCandidateReg() {
+    this.RecruitmentServiceService.GetCandidateRegistration().subscribe(data => {
+      this.joblist = data.filter(x => x.interviewSelected == 1 && x.offered == 0);
+      this.count = this.joblist.length;
+    })
+
   }
-  onRemove(event:any)
-  {
-debugger
-console.log(event);
-this.files.splice(this.files.indexOf(event),1);
+
+  candidateid: any;
+  candidatename: any;
+
+
+  public GetOfferID(id:any, job:any) {
+    this.candidateid = id;
+    this.candidatename = job.candidateName,
+      this.email = job.email
   }
+
+
+  offernotes: any;
+
+
+  attachments = []
+  attachmentsurl: any;
+  brochures = [];
+  imagesurl: any;
+  brochures1 = [];
+  public onattachmentUpload(abcd:any) {
+    debugger;
+    // this.attachments.push(abcd.addedFiles[0]);
+    this.uploadImages();
+    abcd.length = 0;
+  }
+  public uploadImages() {
+    debugger;
+    this.RecruitmentServiceService.UploadImages(this.attachments).subscribe(res => {
+      debugger
+      // this.brochures1.push(res);
+      // let a = this.brochures1[0].slice(2);
+      // this.attachmentsurl = 'http://14.192.17.225' + a;
+
+      this.attachmentsurl.push('assets/Images/pdf.png')
+      Swal.fire("Added Successfully");
+
+    });
+  }
+
+
+
+  public updatedetails() {
+    var entity = {
+      'ID': this.candidateid,
+      'OfferLetterUrl': this.brochures1[0],
+      'OfferNotes': this.offernotes
+    }
+    this.RecruitmentServiceService.UpdateOfferLetter(entity).subscribe(data => {
+      Swal.fire("Candidate Offered Successfully");
+      this.sendmail()
+    })
+  }
+
+  joiningbonus: any;
+  Notes: any;
+  noticeperiodbythen: any;
+
+
+
+
+  public updatejoiningdate() {
+    var entity = {
+      'ID': this.candidateid,
+      'JoiningDate': this.date,
+      'JoiningBonus': this.joiningbonus,
+      'NoticePeriodByThen': this.noticeperiodbythen,
+      'Commentss': this.Notes
+    }
+    this.RecruitmentServiceService.UpdateCandidateJoiningDate(entity).subscribe(data => {
+      Swal.fire("Saved Successfully");
+      this.date = "";
+      this.joiningbonus = "";
+      this
+    })
+  }
+
+
+
+
+  emailattchementurl = [];
+  public email: any;
+  public doctorname: any;
+
+  public sendmail() {
+
+    var entity = {
+      'emailto': this.email,
+      'emailsubject': "Amaze Inc Offer Letter",
+      'emailbody': 'Dear ' + this.candidatename + ',' + "<br><br>" + this.offernotes,
+      'attachmenturl': this.brochures1,
+      'cclist': 0,
+      'bcclist': 0
+    }
+    this.RecruitmentServiceService.sendemail(entity).subscribe(data => {
+    })
+  }
+ 
 }
