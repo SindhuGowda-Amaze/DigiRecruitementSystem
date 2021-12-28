3036cdc4b15c901da116f1b57dbf542bc22417f2
Sindhu Gowda <SindhuGowda.amazeInc@gmail.com>
2021-12-28 18:04:41 +0530

    AUTHDUARD

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { SidebarComponent } from 'src/Pages/sidebar/sidebar.component';
 import { JobRecruitementReportComponent } from './job-recruitement-report/job-recruitement-report.component';
 import { JobRecruitementsComponent } from './job-recruitements/job-recruitements.component';
-import{AppliedCandidatesComponent} from './applied-candidates/applied-candidates.component';
-import{ShortListedCandidatesComponent} from './short-listed-candidates/short-listed-candidates.component';
-import{ScheduledInterviewsComponent} from './scheduled-interviews/scheduled-interviews.component';
-import{SelectedCandidatesComponent} from './selected-candidates/selected-candidates.component';
-import{OfferedCandidatesComponent}from './offered-candidates/offered-candidates.component';
-import{JoinedCandidatesComponent} from './joined-candidates/joined-candidates.component';
-import{DroppedCandidatesComponent}from './dropped-candidates/dropped-candidates.component'
+import { AppliedCandidatesComponent } from './applied-candidates/applied-candidates.component';
+import { ShortListedCandidatesComponent } from './short-listed-candidates/short-listed-candidates.component';
+import { ScheduledInterviewsComponent } from './scheduled-interviews/scheduled-interviews.component';
+import { SelectedCandidatesComponent } from './selected-candidates/selected-candidates.component';
+import { OfferedCandidatesComponent } from './offered-candidates/offered-candidates.component';
+import { JoinedCandidatesComponent } from './joined-candidates/joined-candidates.component';
+import { DroppedCandidatesComponent } from './dropped-candidates/dropped-candidates.component'
 import { ShortlistedCandidatesReportsComponent } from './shortlisted-candidates-reports/shortlisted-candidates-reports.component';
 import { ScheduledInterviewsReportsComponent } from './scheduled-interviews-reports/scheduled-interviews-reports.component';
 import { SelectedCandidatesReportsComponent } from './selected-candidates-reports/selected-candidates-reports.component';
@@ -45,61 +45,63 @@ import { SourcingStaffDashboardComponent } from './sourcing-staff-dashboard/sour
 import { AuthGuard } from './services/auth.guard';
 const routes: Routes = [
 
-  {path: '', redirectTo: 'Login', pathMatch: 'full' },
-  {path: "Login", component: LoginPageComponent },
-  {path: "Header", component: HeaderComponent },
-  {path: "Sidebar", component: SidebarComponent},
-  {path:'Dashboard',component:DashboardComponent, canActivate: [AuthGuard]},
-  {path:'JobRecruitementReport',component:JobRecruitementReportComponent},
-  {path:'JobRecruitements',component:JobRecruitementsComponent},
- {path:'AppliedCandidates',component:AppliedCandidatesComponent},
- {path:'ShortListedCandidates',component:ShortListedCandidatesComponent},
- {path:'ScheduledInterviews',component:ScheduledInterviewsComponent},
- {path:'SelectedCandidates',component:SelectedCandidatesComponent},
- {path:'OfferedCandidates',component:OfferedCandidatesComponent},
- {path:'JoinedCandidates',component:JoinedCandidatesComponent},
- {path:'DroppedCandidates',component: DroppedCandidatesComponent},
-  {path:'AppliedCandidatesReports',component:AppliedCandidatesReportsComponent},
-  {path:'ShortlistedCandidatesReports',component:ShortlistedCandidatesReportsComponent},
-  {path:'ScheduledInterviewsReports',component: ScheduledInterviewsReportsComponent},
-  {path:'SelectedCandidatesReports',component: SelectedCandidatesReportsComponent},
-  {path:'OfferedCandidatesReports',component: OfferedCandidatesReportsComponent},
-  {path:'DroppedCandiadtesReports',component: DroppedCandiadtesReportsComponent},
-  {path:'JoinedCandidatesReport',component: JoinedCandidatesReportComponent},
-  {path:'JobRequisitionForm',component: JobRequisitionFormComponent},
-  {path:'Footer',component:FooterComponent},
-  {path:'JobVacancies',component:JobVacanciesComponent},
-   {path:'JobVacancies/:id',component:JobVacanciesComponent},
-  {path:'Registration',component:RegistrationComponent},
-  {path:'JobRequisition',component:JobRequisitionComponent},
-  {path:'VendorJobOpenings',component:VendorJobOpeningsComponent},
-  { path: 'ClientDashBoard', component: ClientdashboardComponent ,canActivate: [AuthGuard]},
-  { path: 'ClientForm', component: ClientformComponent ,canActivate: [AuthGuard]},
-  { path: 'ClientForm/:id', component: ClientformComponent ,canActivate: [AuthGuard]},
-  { path: 'ClientStaffDashBoard', component: ClientStaffDashboardComponent ,canActivate: [AuthGuard]},
-  { path: 'ClientStaffForm', component: ClientStaffFormComponent ,canActivate: [AuthGuard]},
+  { path: '', redirectTo: 'Login', pathMatch: 'full' },
+  { path: "Login", component: LoginPageComponent },
+  { path: "Header", component: HeaderComponent, canActivate: [AuthGuard] },
+  { path: "Sidebar", component: SidebarComponent, canActivate: [AuthGuard] },
+  { path: 'Dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'JobRecruitementReport', component: JobRecruitementReportComponent, canActivate: [AuthGuard] },
+  { path: 'JobRecruitements', component: JobRecruitementsComponent, canActivate: [AuthGuard] },
+  { path: 'AppliedCandidates', component: AppliedCandidatesComponent, canActivate: [AuthGuard] },
+  { path: 'ShortListedCandidates', component: ShortListedCandidatesComponent, canActivate: [AuthGuard] },
+  { path: 'ScheduledInterviews', component: ScheduledInterviewsComponent, canActivate: [AuthGuard] },
+  { path: 'SelectedCandidates', component: SelectedCandidatesComponent, canActivate: [AuthGuard] },
+  { path: 'OfferedCandidates', component: OfferedCandidatesComponent, canActivate: [AuthGuard] },
+  { path: 'JoinedCandidates', component: JoinedCandidatesComponent, canActivate: [AuthGuard] },
+  { path: 'DroppedCandidates', component: DroppedCandidatesComponent, canActivate: [AuthGuard] },
+  { path: 'AppliedCandidatesReports', component: AppliedCandidatesReportsComponent, canActivate: [AuthGuard] },
+  { path: 'ShortlistedCandidatesReports', component: ShortlistedCandidatesReportsComponent, canActivate: [AuthGuard] },
+  { path: 'ScheduledInterviewsReports', component: ScheduledInterviewsReportsComponent, canActivate: [AuthGuard] },
+  { path: 'SelectedCandidatesReports', component: SelectedCandidatesReportsComponent, canActivate: [AuthGuard] },
+  { path: 'OfferedCandidatesReports', component: OfferedCandidatesReportsComponent, canActivate: [AuthGuard] },
+  { path: 'DroppedCandiadtesReports', component: DroppedCandiadtesReportsComponent, canActivate: [AuthGuard] },
+  { path: 'JoinedCandidatesReport', component: JoinedCandidatesReportComponent, canActivate: [AuthGuard] },
+  { path: 'JobRequisitionForm', component: JobRequisitionFormComponent, canActivate: [AuthGuard] },
+  { path: 'Footer', component: FooterComponent, canActivate: [AuthGuard] },
+  { path: 'JobVacancies', component: JobVacanciesComponent, canActivate: [AuthGuard] },
+  { path: 'JobVacancies/:id', component: JobVacanciesComponent, canActivate: [AuthGuard] },
+  { path: 'Registration', component: RegistrationComponent, canActivate: [AuthGuard] },
+  { path: 'JobRequisition', component: JobRequisitionComponent, canActivate: [AuthGuard] },
+  { path: 'VendorJobOpenings', component: VendorJobOpeningsComponent, canActivate: [AuthGuard] },
+  { path: 'ClientDashBoard', component: ClientdashboardComponent, canActivate: [AuthGuard] },
+  { path: 'ClientForm', component: ClientformComponent, canActivate: [AuthGuard] },
+  { path: 'ClientForm/:id', component: ClientformComponent, canActivate: [AuthGuard] },
+  { path: 'ClientStaffDashBoard', component: ClientStaffDashboardComponent, canActivate: [AuthGuard] },
+  { path: 'ClientStaffForm', component: ClientStaffFormComponent, canActivate: [AuthGuard] },
 
-  {path:'VendorDashboard',component:VendorDashboardComponent,canActivate: [AuthGuard]},
-  {path:'VendorForm',component: VendorFormComponent,canActivate: [AuthGuard]},
-  { path: 'RecruiterForm', component: RecruiterFormComponent },
-  { path: 'RecruiterForm/:id', component: RecruiterFormComponent },
-  { path: 'RecruiterDashboard', component: RecruiterDashboardComponent },
-  { path: 'RecruiterStaff', component: RecruiterStaffComponent },
-  { path: 'RecruiterStaff/:id', component: RecruiterStaffComponent },
-  { path: 'RecruiterStaffDashboard', component: RecruiterStaffDashboardComponent },
-  {path:'VendorStaffDashboard',component:VendorStaffDashboardComponent,canActivate: [AuthGuard]},
-  {path:'VendorStaffForm',component:VendorStaffFormComponent,canActivate: [AuthGuard]},
-  {path:'VendorStaffForm/:id',component:VendorStaffFormComponent,},
-  { path: 'SourcingDashboard', component: SourcingDashboardComponent },
-  { path: 'SourcingForm', component: SourcingFormComponent },
-  { path: 'SourcingForm/:id', component: SourcingFormComponent },
-  { path: 'SourcingStaffForm', component: SourcingStaffFormComponent },
-  { path: 'SourcingStaffDashboard', component: SourcingStaffDashboardComponent },
-  {path:'VendorForm/:id',component: VendorFormComponent},
-  { path: 'SourcingStaffForm/:id', component: SourcingStaffFormComponent },
-  { path: 'SourcingStaffDashboard', component: SourcingStaffDashboardComponent }, 
-  { path: 'SourcingStaffDashboard', component: SourcingStaffDashboardComponent },
-  { path: 'ClientStaffForm/:id', component: ClientStaffFormComponent, canActivate: [AuthGuard]},
+  { path: 'VendorDashboard', component: VendorDashboardComponent, canActivate: [AuthGuard] },
+  { path: 'VendorForm', component: VendorFormComponent, canActivate: [AuthGuard] },
+  { path: 'RecruiterForm', component: RecruiterFormComponent, canActivate: [AuthGuard] },
+  { path: 'RecruiterForm/:id', component: RecruiterFormComponent, canActivate: [AuthGuard] },
+  { path: 'RecruiterDashboard', component: RecruiterDashboardComponent, canActivate: [AuthGuard] },
+  { path: 'RecruiterStaff', component: RecruiterStaffComponent, canActivate: [AuthGuard] },
+  { path: 'RecruiterStaff/:id', component: RecruiterStaffComponent, canActivate: [AuthGuard] },
+  { path: 'RecruiterStaffDashboard', component: RecruiterStaffDashboardComponent, canActivate: [AuthGuard] },
+
+  { path: 'VendorStaffDashboard', component: VendorStaffDashboardComponent, canActivate: [AuthGuard] },
+  { path: 'VendorStaffForm', component: VendorStaffFormComponent, canActivate: [AuthGuard] },
+  { path: 'VendorStaffForm/:id', component: VendorStaffFormComponent, canActivate: [AuthGuard] },
+  { path: 'SourcingDashboard', component: SourcingDashboardComponent, canActivate: [AuthGuard] },
+  { path: 'SourcingForm', component: SourcingFormComponent, canActivate: [AuthGuard] },
+  { path: 'SourcingForm/:id', component: SourcingFormComponent, canActivate: [AuthGuard] },
+  { path: 'SourcingStaffForm', component: SourcingStaffFormComponent, canActivate: [AuthGuard] },
+  { path: 'SourcingStaffDashboard', component: SourcingStaffDashboardComponent, canActivate: [AuthGuard] },
+  { path: 'VendorForm/:id', component: VendorFormComponent, canActivate: [AuthGuard] },
+  { path: 'SourcingStaffForm/:id', component: SourcingStaffFormComponent, canActivate: [AuthGuard] },
+  { path: 'SourcingStaffDashboard', component: SourcingStaffDashboardComponent, canActivate: [AuthGuard] },
+
+  { path: 'SourcingStaffDashboard', component: SourcingStaffDashboardComponent, canActivate: [AuthGuard] },
+  { path: 'ClientStaffForm/:id', component: ClientStaffFormComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
