c12a24338e103ff99548a32036af19368d54d72f
PraveenKumarVelu28 <praveenkumaramze@gmail.com>
2022-02-03 15:42:31 +0530

    commit all

--- src/Pages/header/header.component.ts
+++ src/Pages/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -14,13 +15,45 @@ export class HeaderComponent implements OnInit {
   roleid:any;
   role:any;
   UserName:any;
+  pagename: any;
+  showsidebar: any;
+  time: any;
+  hh: any;
+  mm: any;
+  ampm: any;
+  username: any
   ngOnInit() {
+
+
+    interval(1000).subscribe((x => {
+      this.pagename = localStorage.getItem('Pagename')
+
+
+    }));
+
+    
     this.temp=sessionStorage.getItem('temp')
     this.roleid = sessionStorage.getItem('roleid');
     this.company_name = sessionStorage.getItem("company_name");
     this.UserName = sessionStorage.getItem('UserName');
     this.role = sessionStorage.getItem('role')
+
+    
+
+    setInterval(() => {
+      var time = new Date();
+      this.time = time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+      let temp: any = this.time.split(':');
+      this.hh = temp[0];
+      let temp1: any = this.time.split(':')[1].split(" ");
+      this.mm = temp1[0];
+      this.ampm = temp1[1];
+    }, 1000);
+
+
   }
+
+  
 
   logout() {
     sessionStorage.clear();
--- src/Pages/sidebar/sidebar.component.ts
+++ src/Pages/sidebar/sidebar.component.ts
@@ -28,33 +28,753 @@ export class SidebarComponent implements OnInit {
     location.reload();
   }
 
+
+  public openCity(evt: any) {
+    var i, tablinks;
+
+    tablinks = document.getElementsByClassName("nonactive");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    evt.currentTarget.className += " active";
+  }
+  client:any
+public Client(){
+  localStorage.setItem('Pagename', 'Client')
+    this.router.navigate(['/ClientDashBoard']);
+    this.client=true
+    this.home=false
+    this.clientstaff=false
+    this.recruitstaff = false
+    this.vendorstaf=false
+    this.vendo=false
+
+    
+}
+clientstaff:any
+public Clientstaff(){
+  localStorage.setItem('Pagename', 'Client Staff')
+    this.router.navigate(['/ClientStaffDashBoard']);
+    this.client=false
+    this.clientstaff=true
+    this.home=false
+    this.recruitstaff = false
+    this.vendorstaf=false
+    this.vendo=false
+}
+recruitstaff:any
+public Recruitstaff(){
+  localStorage.setItem('Pagename', 'Client Staff')
+  this.router.navigate(['/ClientStaffDashBoard']);
+  this.client=false
+  this.clientstaff=false
+  this.home=false
+  this.recruitstaff = true
+  this.vendorstaf=false
+  this.vendo=false
+}
+vendo:any
+public vendor(){
+  localStorage.setItem('Pagename', 'Client Staff')
+  this.router.navigate(['/ClientStaffDashBoard']);
+  this.vendo=true
+  this.client=false
+  this.clientstaff=false
+  this.home=false
+  this.recruitstaff = false
+  this.vendorstaf=false
+}
+
+vendorstaf:any
+public vendorstaff(){
+  localStorage.setItem('Pagename', 'Client Staff')
+  this.router.navigate(['/VendorStaffDashboard']);
+  this.vendorstaf=true
+  this.vendo=false
+  this.client=false
+  this.clientstaff=false
+  this.home=false
+  this.recruitstaff = false
+}
+
 home:any
+Vendorrecruit:any
   public admin() {
     debugger
     if (this.roleid=='1') {
     localStorage.setItem('Pagename', 'Dashboard')
     this.router.navigate(['/AdminDashboard']);
     this.home=true
+    this.Jobrecruit=false
+    this.Vendorrecruit=false
+   
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.Vendorrecruit=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.selected=false
+    this.offered=false
+    this.joined=false
+    this.dropped=false
+    this.vendorreport=false
+    this.appliedreport=false
+    this.shortlistedreport=false
+    this.selectedreport=false
+    this.offeredreport=false
+    this.Jobrecruitreport=false
+    this.joinedreport=false
+    this.droppedreport=false
     }
     else if( this.roleid!='5')
     {
       localStorage.setItem('Pagename', 'Dashboard')
       this.router.navigate(['/AdminDashboard']);
       this.home=true
+      this.Jobrecruit=false
+      this.Vendorrecruit=false
+     
+      this.applied=false
+      this.shortlist=false
+      this.scheduled=false
+      this.Vendorrecruit=false
+      this.applied=false
+      this.shortlist=false
+      this.scheduled=false
+      this.selected=false
+      this.offered=false
+      this.joined=false
+      this.dropped=false
+      this.vendorreport=false
+      this.appliedreport=false
+      this.shortlistedreport=false
+      this.selectedreport=false
+      this.offeredreport=false
+      this.Jobrecruitreport=false
+      this.joinedreport=false
+      this.droppedreport=false
     }
     else if(this.roleid=='6'){
       localStorage.setItem('Pagename', 'Dashboard')
       this.router.navigate(['/Dashboard']);
       this.home=true
+      this.Jobrecruit=false
+      this.Vendorrecruit=false
+     
+      this.applied=false
+      this.shortlist=false
+      this.scheduled=false
+      this.Vendorrecruit=false
+      this.applied=false
+      this.shortlist=false
+      this.scheduled=false
+      this.selected=false
+      this.offered=false
+      this.joined=false
+      this.dropped=false
+      this.vendorreport=false
+      this.appliedreport=false
+      this.shortlistedreport=false
+      this.selectedreport=false
+      this.offeredreport=false
+      this.Jobrecruitreport=false
+      this.joinedreport=false
+      this.droppedreport=false
     }
     else if(this.roleid=='3'){
       localStorage.setItem('Pagename', 'Dashboard')
       this.router.navigate(['/Dashboard']);
       this.home=true
+      this.Jobrecruit=false
+      this.Vendorrecruit=false
+     
+      this.applied=false
+      this.shortlist=false
+      this.scheduled=false
+      this.Vendorrecruit=false
+      this.applied=false
+      this.shortlist=false
+      this.scheduled=false
+      this.selected=false
+      this.offered=false
+      this.joined=false
+      this.dropped=false
+      this.vendorreport=false
+      this.appliedreport=false
+      this.shortlistedreport=false
+      this.selectedreport=false
+      this.offeredreport=false
+      this.Jobrecruitreport=false
+      this.joinedreport=false
+      this.droppedreport=false
     }
     else if(this.roleid=='1'){
       localStorage.setItem('Pagename', 'Dashboard')
       this.router.navigate(['/Dashboard']);
+      this.Jobrecruit=false
+      this.Vendorrecruit=false
+      this.home=true
+      this.applied=false
+      this.shortlist=false
+      this.scheduled=false
+      this.Vendorrecruit=false
+      this.applied=false
+      this.shortlist=false
+      this.scheduled=false
+      this.selected=false
+      this.offered=false
+      this.joined=false
+      this.dropped=false
+      this.vendorreport=false
+      this.appliedreport=false
+      this.shortlistedreport=false
+      this.selectedreport=false
+      this.offeredreport=false
+      this.Jobrecruitreport=false
+      this.joinedreport=false
+      this.droppedreport=false
     }
   }
+
+  Jobrecruit:any
+  applied:any
+  shortlist:any
+  public JobRecruitment(){
+    this.Jobrecruit=true
+    this.Vendorrecruit=false
+    this.home=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.Vendorrecruit=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.selected=false
+    this.offered=false
+    this.joined=false
+    this.dropped=false
+    this.vendorreport=false
+    this.appliedreport=false
+    this.shortlistedreport=false
+    this.selectedreport=false
+    this.offeredreport=false
+    this.Jobrecruitreport=false
+    this.joinedreport=false
+    this.droppedreport=false
+    localStorage.setItem('Pagename', 'Job Recruitment')
+    this.router.navigate(['/JobRecruitements']);
+   
+
+
+  }
+
+  public VendorRecruitment(){
+    this.Vendorrecruit=true
+    this.Jobrecruit=false
+   
+    this.home=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+  
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.selected=false
+    this.offered=false
+    this.joined=false
+    this.dropped=false
+    this.vendorreport=false
+    this.appliedreport=false
+    this.shortlistedreport=false
+    this.selectedreport=false
+    this.offeredreport=false
+    this.Jobrecruitreport=false
+    this.joinedreport=false
+    this.droppedreport=false
+    localStorage.setItem('Pagename', 'Job Recruitment')
+    this.router.navigate(['/VendorJobOpenings']);
+  
+  }
+
+  public Appliedcandidates(){
+    localStorage.setItem('Pagename', 'Applied Candidates')
+    this.router.navigate(['/AppliedCandidates']);
+    this.applied=true
+    this.Jobrecruit=false
+    this.Vendorrecruit=false
+    this.home=false
+  
+    this.shortlist=false
+    this.scheduled=false
+    this.Vendorrecruit=false
+  
+    this.shortlist=false
+    this.scheduled=false
+    this.selected=false
+    this.offered=false
+    this.joined=false
+    this.dropped=false
+    this.vendorreport=false
+    this.appliedreport=false
+    this.shortlistedreport=false
+    this.selectedreport=false
+    this.offeredreport=false
+    this.Jobrecruitreport=false
+    this.joinedreport=false
+    this.droppedreport=false
+  }
+
+
+  public Shortlisted(){
+    localStorage.setItem('Pagename', 'Shortlisted Candidates')
+    this.router.navigate(['/ShortListedCandidates']);
+    this.shortlist=true
+    this.applied=false
+    this.Jobrecruit=false
+    this.Vendorrecruit=false
+    this.home=false
+  
+
+    this.scheduled=false
+    this.Vendorrecruit=false
+  
+ 
+    this.scheduled=false
+    this.selected=false
+    this.offered=false
+    this.joined=false
+    this.dropped=false
+    this.vendorreport=false
+    this.appliedreport=false
+    this.shortlistedreport=false
+    this.selectedreport=false
+    this.offeredreport=false
+    this.Jobrecruitreport=false
+    this.joinedreport=false
+    this.droppedreport=false
+  }
+
+  scheduled:any
+  selected:any
+  offered:any
+  joined:any
+  dropped:any
+  public Scheduled(){
+    localStorage.setItem('Pagename', 'Scheduled Candidates')
+    this.router.navigate(['/ScheduledInterviews']);
+    this.scheduled=true
+    this.Jobrecruit=false
+    this.Vendorrecruit=false
+    this.home=false
+    this.applied=false
+    this.shortlist=false
+   
+    this.Vendorrecruit=false
+    this.applied=false
+    this.shortlist=false
+   
+    this.selected=false
+    this.offered=false
+    this.joined=false
+    this.dropped=false
+    this.vendorreport=false
+    this.appliedreport=false
+    this.shortlistedreport=false
+    this.selectedreport=false
+    this.offeredreport=false
+    this.Jobrecruitreport=false
+    this.joinedreport=false
+    this.droppedreport=false
+  }
+  
+
+  public Selected(){
+    localStorage.setItem('Pagename', 'Selected Candidates')
+    this.router.navigate(['/SelectedCandidates']);
+    this.selected=true
+    this.Jobrecruit=false
+    this.Vendorrecruit=false
+    this.home=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.Vendorrecruit=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+  
+    this.offered=false
+    this.joined=false
+    this.dropped=false
+    this.vendorreport=false
+    this.appliedreport=false
+    this.shortlistedreport=false
+    this.selectedreport=false
+    this.offeredreport=false
+    this.Jobrecruitreport=false
+    this.joinedreport=false
+    this.droppedreport=false
+  }
+
+
+  public Offered(){
+    localStorage.setItem('Pagename', 'Offered Candidates')
+    this.router.navigate(['/OfferedCandidates']);
+    this.offered=true
+    this.Jobrecruit=false
+    this.Vendorrecruit=false
+    this.home=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.Vendorrecruit=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.selected=false
+  
+    this.joined=false
+    this.dropped=false
+    this.vendorreport=false
+    this.appliedreport=false
+    this.shortlistedreport=false
+    this.selectedreport=false
+    this.offeredreport=false
+    this.Jobrecruitreport=false
+    this.joinedreport=false
+    this.droppedreport=false
+  }
+
+  public Joined(){
+    localStorage.setItem('Pagename', 'Joined Candidates')
+    this.router.navigate(['/JoinedCandidates']);
+    this.joined=true
+    this.Jobrecruit=false
+    this.Vendorrecruit=false
+    this.home=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.Vendorrecruit=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.selected=false
+    this.offered=false
+  
+    this.dropped=false
+    this.vendorreport=false
+    this.appliedreport=false
+    this.shortlistedreport=false
+    this.selectedreport=false
+    this.offeredreport=false
+    this.Jobrecruitreport=false
+    this.joinedreport=false
+    this.droppedreport=false
+  }
+
+
+  public Dropped(){
+    localStorage.setItem('Pagename', 'Dropped Candidates')
+    this.router.navigate(['/DroppedCandidates']);
+    this.dropped=true
+    this.Jobrecruit=false
+    this.Vendorrecruit=false
+    this.home=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.Vendorrecruit=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.selected=false
+    this.offered=false
+    this.joined=false
+  
+    this.vendorreport=false
+    this.appliedreport=false
+    this.shortlistedreport=false
+    this.selectedreport=false
+    this.offeredreport=false
+    this.Jobrecruitreport=false
+    this.joinedreport=false
+    this.droppedreport=false
+
+  }
+  vendorreport:any
+  appliedreport:any
+  shortlistedreport:any
+  scheduledreport:any
+  selectedreport:any
+  offeredreport:any
+  Jobrecruitreport:any
+  joinedreport:any
+  droppedreport:any
+  public VendorReport(){
+    localStorage.setItem('Pagename', 'Vendor')
+    this.router.navigate(['/VendorJobOpenings']);
+    this.vendorreport=true
+    this.Jobrecruit=false
+    this.Vendorrecruit=false
+    this.home=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.Vendorrecruit=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.selected=false
+    this.offered=false
+    this.joined=false
+    this.dropped=false
+  
+    this.appliedreport=false
+    this.shortlistedreport=false
+    this.selectedreport=false
+    this.offeredreport=false
+    this.Jobrecruitreport=false
+    this.joinedreport=false
+    this.droppedreport=false
+
+  }
+  public AppliedReport(){
+    localStorage.setItem('Pagename', 'Applied')
+    this.router.navigate(['/AppliedCandidatesReports']);
+    this.appliedreport=true
+    this.Jobrecruit=false
+    this.Vendorrecruit=false
+    this.home=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.Vendorrecruit=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.selected=false
+    this.offered=false
+    this.joined=false
+    this.dropped=false
+    this.vendorreport=false
+   
+    this.shortlistedreport=false
+    this.selectedreport=false
+    this.offeredreport=false
+    this.Jobrecruitreport=false
+    this.joinedreport=false
+    this.droppedreport=false
+
+  }
+
+  public Shortlistedreport(){
+    localStorage.setItem('Pagename', 'Shortlisted')
+    this.router.navigate(['/ShortlistedCandidatesReports']);
+    
+    this.shortlistedreport=true
+    this.Jobrecruit=false
+    this.Vendorrecruit=false
+    this.home=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.Vendorrecruit=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.selected=false
+    this.offered=false
+    this.joined=false
+    this.dropped=false
+    this.vendorreport=false
+    this.appliedreport=false
+   
+    this.selectedreport=false
+    this.offeredreport=false
+    this.Jobrecruitreport=false
+    this.joinedreport=false
+    this.droppedreport=false
+
+  }
+
+  public ScheduledReport(){
+    localStorage.setItem('Pagename', 'Scheduled')
+    this.router.navigate(['/ScheduledInterviewsReports']);
+    this.scheduledreport=true
+    this.Jobrecruit=false
+    this.Vendorrecruit=false
+    this.home=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.Vendorrecruit=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.selected=false
+    this.offered=false
+    this.joined=false
+    this.dropped=false
+    this.vendorreport=false
+    this.appliedreport=false
+    this.shortlistedreport=false
+    this.selectedreport=false
+    this.offeredreport=false
+    this.Jobrecruitreport=false
+    this.joinedreport=false
+    this.droppedreport=false
+
+  }
+
+  public SelectedReport(){
+    localStorage.setItem('Pagename', 'Selected')
+    this.router.navigate(['/SelectedCandidatesReports']);
+    this.selectedreport=true
+    this.Jobrecruit=false
+    this.Vendorrecruit=false
+    this.home=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.Vendorrecruit=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+  
+    this.offered=false
+    this.joined=false
+    this.dropped=false
+    this.vendorreport=false
+    this.appliedreport=false
+    this.shortlistedreport=false
+    this.selectedreport=false
+    this.offeredreport=false
+    this.Jobrecruitreport=false
+    this.joinedreport=false
+    this.droppedreport=false
+
+  }
+
+  public OfferedReport(){
+    localStorage.setItem('Pagename', 'Job Recruitment')
+    this.router.navigate(['/OfferedCandidatesReports']);
+    this.offeredreport=true
+    this.Jobrecruit=false
+    this.Vendorrecruit=false
+    this.home=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.Vendorrecruit=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.selected=false
+    this.offered=false
+    this.joined=false
+    this.dropped=false
+    this.vendorreport=false
+    this.appliedreport=false
+    this.shortlistedreport=false
+    this.selectedreport=false
+ 
+    this.Jobrecruitreport=false
+    this.joinedreport=false
+    this.droppedreport=false
+
+  }
+
+  public JobRecruitReport(){
+    localStorage.setItem('Pagename', 'Job Recruitment')
+    this.router.navigate(['/JobRecruitementReport']);
+    this.Jobrecruitreport=true
+    this.Jobrecruit=false
+    this.Vendorrecruit=false
+    this.home=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.Vendorrecruit=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.selected=false
+    this.offered=false
+    this.joined=false
+    this.dropped=false
+    this.vendorreport=false
+    this.appliedreport=false
+    this.shortlistedreport=false
+    this.selectedreport=false
+    this.offeredreport=false
+  
+    this.joinedreport=false
+    this.droppedreport=false
+
+  }
+
+  public JoinedReport(){
+    localStorage.setItem('Pagename', 'Joined')
+    this.router.navigate(['/JoinedCandidatesReport']);
+    this.joinedreport=true
+    this.Jobrecruit=false
+    this.Vendorrecruit=false
+    this.home=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.Vendorrecruit=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.selected=false
+    this.offered=false
+    this.joined=false
+    this.dropped=false
+    this.vendorreport=false
+    this.appliedreport=false
+    this.shortlistedreport=false
+    this.selectedreport=false
+    this.offeredreport=false
+    this.Jobrecruitreport=false
+   
+    this.droppedreport=false
+
+  }
+
+  public DroppedReport(){
+    localStorage.setItem('Pagename', 'Dropped')
+    this.router.navigate(['/DroppedCandiadtesReports']);
+    this.droppedreport=true
+    this.Jobrecruit=false
+    this.Vendorrecruit=false
+    this.home=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.Vendorrecruit=false
+    this.applied=false
+    this.shortlist=false
+    this.scheduled=false
+    this.selected=false
+    this.offered=false
+    this.joined=false
+    this.dropped=false
+    this.vendorreport=false
+    this.appliedreport=false
+    this.shortlistedreport=false
+    this.selectedreport=false
+    this.offeredreport=false
+    this.Jobrecruitreport=false
+    this.joinedreport=false
+   
+
+  }
+
 }
